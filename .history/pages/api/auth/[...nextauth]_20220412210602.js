17a0ee28d9cb1774927d3655a809eb795180a1d2
AiriShigemizu <jcfamily.ai@gmail.com>
2022-04-12 21:50:29 +0900

    delete callback

--- .history/pages/api/auth/[...nextauth]_20220412210602.js
+++ .history/pages/api/auth/[...nextauth]_20220412210602.js
@@ -12,6 +12,19 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      const isAllowedToSignIn = true
+      if (isAllowedToSignIn) {
+        return true
+      } else {
+        // Return false to display a default error message
+        return false
+        // Or you can return a URL to redirect to:
+        // return '/unauthorized'
+      }
+    }
+  },
   secret: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET,
   adapter: FirebaseAdapter({
     db: db,
